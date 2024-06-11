427b57af7599af4c7e8895de06445187d052e410
Dinmukhammed <arystambekdimash005@gmail.com>
2024-06-12 00:23:29 +0500

    first commit

---
+++ src/events/event-controller.ts
@@ -0,0 +1,61 @@
+import { Request, Response } from 'express';
+import { CreateEventDto } from './dtos/CreateEvent.dot';
+import EventService from './event-service';
+
+class EventController {
+    private eventService : EventService;
+
+
+    constructor(eventService : EventService){
+        this.eventService = eventService;
+    }
+
+    createEvent = async (req: Request, res: Response): Promise<void> => {
+        try {
+          const createEventDto: CreateEventDto = req.body;
+          console.log(req.body)
+          const event = await this.eventService.createEvent(createEventDto);
+          res.status(201).json(event);
+        } catch (error: any) {
+          res.status(500).send({ error: error.message });
+        }
+      }
+
+
+
+    getEvents = async (req: Request, res: Response): Promise<void> => {
+        try {
+          const events = await this.eventService.getEvents();
+          res.status(200).json(events);
+        } catch (error: any) {
+          res.status(500).send({ error: error.message });
+        }
+      }
+
+    getEventById = async (req: Request, res: Response): Promise<void> => {
+        try {
+          const { id } = req.params;
+          const event = await this.eventService.getEventById(id);
+          if (!event) {
+            res.status(404).json({ message: 'Event not found' });
+            return;
+          }
+          res.status(200).json(event);
+        } catch (error: any) {
+          res.status(500).send({ error: error.message });
+        }
+      }
+
+      getEventsByLocation = async (req: Request, res: Response): Promise<void> => {
+        try {
+            const user = (req as any).user;
+            const location = user.location;
+            const events = await this.eventService.getEventsByLocation(location);
+            res.status(200).json(events);
+        } catch (error: any) {
+            res.status(500).send({ error: error.message });
+        }
+    }
+}
+
+export default EventController;
